1ff380961809b14a0bf9dc068b198cd5743adb91
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2024-11-03 07:52:06 -0300

    fix config images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '18.221.8.21',
+        port: '8080',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8080',
+
+      },
+    ],
   },
 };
 
