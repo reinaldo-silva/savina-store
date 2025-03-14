23ec0dca64e2d5bba85b1fc733f999b318e80f56
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2025-03-14 10:56:26 -0300

    fix build

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import { Footer } from "@/components/Footer";
 import { FooterNavigation } from "@/components/FooterNavigation";
 import { Header } from "@/components/Header";
 import { getCategories } from "@/services/categoriesService";
-import { use } from "react";
+import { Suspense, use } from "react";
 import { RootContextProvider } from "./RootContext";
 
 export default function RootLayout({
@@ -15,7 +15,9 @@ export default function RootLayout({
   return (
     <RootContextProvider>
       <div className="max-h-vh flex min-h-screen w-screen flex-col overflow-x-hidden bg-zinc-50 pb-[60px] pt-[104px] md:pb-0 md:pt-[133px]">
-        <Header categories={(allCategories.data ?? []).slice(0, 4)} />
+        <Suspense>
+          <Header categories={(allCategories.data ?? []).slice(0, 4)} />
+        </Suspense>
 
         <div className="mx-auto flex h-full w-full max-w-screen-lg flex-1 border-x border-zinc-200 bg-white shadow-sm">
           {children}
