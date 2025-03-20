58b19fe24bf335148a1bed4618dea833185a2e7e
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2025-03-20 14:21:36 -0300

    add social links

--- src/components/pages/catalog/PreviewProduct.tsx
+++ src/components/pages/catalog/PreviewProduct.tsx
@@ -7,7 +7,12 @@ import { useClickOutside } from "@/hook/useClickOutside";
 import { useLoading } from "@/hook/useLoading";
 import { getProductBySlug, Product } from "@/services/productService";
 import { formatPrice } from "@/utils/formatPrice";
-import { CircleNotch, X } from "@phosphor-icons/react";
+import {
+  CircleNotch,
+  InstagramLogo,
+  WhatsappLogo,
+  X,
+} from "@phosphor-icons/react";
 import { animated, useSpring } from "@react-spring/web";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
@@ -32,6 +37,10 @@ export function PreviewProduct() {
     config: { duration: 200 },
   });
 
+  const phoneNumber = "5517991115889";
+  const message = `Olá, tenho interesse nesse produto: ${window.location.href}`;
+  const encodedMessage = encodeURIComponent(message);
+
   useEffect(() => {
     if (id) {
       start();
@@ -51,7 +60,7 @@ export function PreviewProduct() {
       <div className="absolute left-0 top-0 h-full w-full bg-zinc-950/40" />
       <CardDefault
         ref={divRef}
-        className="relative z-10 flex max-w-screen-sm flex-col bg-zinc-50 p-2"
+        className="relative z-10 flex h-auto max-h-[100%] w-full max-w-screen-sm flex-col overflow-y-scroll bg-red-50 p-2 md:size-auto"
       >
         {!isLoading && (
           <Link href={`/catalog`} className="absolute right-3 top-3">
@@ -77,17 +86,33 @@ export function PreviewProduct() {
                 <div className="relative flex max-h-[30px] min-h-[300px] items-center overflow-hidden sm:max-h-[360px] sm:min-h-[360px]">
                   <CarouselSection images={product.images} isCatalog />
                 </div>
+                <div className="flex items-center justify-between">
+                  <Heading as="h1" className="my-4">
+                    {formatPrice(product.price)}
+                  </Heading>
 
-                <Heading as="h1" className="my-4">
-                  {formatPrice(product.price)}
-                </Heading>
+                  <div className="flex items-center gap-1">
+                    <Link
+                      className="self-start p-1"
+                      target="_blank"
+                      href={`https://wa.me/${phoneNumber}?text=${encodedMessage}`}
+                    >
+                      <WhatsappLogo weight="bold" size={32} />
+                    </Link>
+                    <Link
+                      className="self-start p-1"
+                      target="_blank"
+                      href={`http://instagram.com/savina.acessorios`}
+                    >
+                      <InstagramLogo weight="bold" size={32} />
+                    </Link>
+                  </div>
+                </div>
 
                 <Heading as="h5" className="font-semibold">
                   Descrição do produto
                 </Heading>
-                <Text className="line-clamp-5 leading-relaxed">
-                  {product.description}
-                </Text>
+                <Text className="leading-relaxed">{product.description}</Text>
               </>
             )}
           </>
